057b7ad762c8c03fb2571e1f19475a2ba15e56d2
Ode Manzi <manzii.ode@gmail.com>
2018-10-28 16:07:43 +0800

    Styled up mobile navbar and added media queries mixins

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,12 @@
-console.log("God Bless!");
+// Navigation
+
+var navigationWrapper = document.querySelector('.navigation'),
+    menuWrapper = document.querySelector('.menu'),
+    subMenuTriggers = document.querySelectorAll('li.menu__item--with-submenu');
+
+subMenuTriggers.forEach(function (element) {
+    element.addEventListener('click', function (e) {
+        console.log(e.target.parentNode);
+    })
+
+})
--- src/scripts/modules/Menu.js
+++ src/scripts/modules/Menu.js
@@ -1,4 +1,26 @@
-const $=require('jquery')
-$(document).ready(() => {
-    console.log('Ready!!')
-})
+class Menu {
+    constructor(navWrapper, subnavWapper) {
+        this.navWrapper = navWrapper;
+        this.subnavWapper = subnavWapper;
+        this.triggers = document.querySelectorAll('.menu__item--with-submenu');
+        this.events();
+    }
+
+    events() {
+        this.triggers.forEach((item) => {
+            // item.addEventListener('mouseover', this.revealSubMenu.bind(this));
+            item.addEventListener('mouseover', function (e) {
+                // e.stopImmediatePropagation();
+                console.log(this.querySelector('.submenu'))
+                this.querySelector('.submenu').classList['toggle']('acitve');
+            })
+        })
+
+    }
+
+    revealSubMenu() {
+        console.log(this)
+    }
+}
+
+export default Menu;
