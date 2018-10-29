6e50d5c0971524214065f5b980c1e67eccfa6b62
Ode Manzi <manzii.ode@gmail.com>
2018-10-29 16:26:22 +0800

    removed conflicting files

--- src/scripts/app.js
+++
@@ -1,8 +0,0 @@
-var menuOpenButton = document.querySelector('.menu__button'),
-    mobileMenu = document.querySelector('.mobile-nav');
-
-menuOpenButton.addEventListener('click', toggle);
-
-function toggle() {
-    mobileMenu.classList['add']('is-visible');
-}
