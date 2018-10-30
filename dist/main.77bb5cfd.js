d90022ed32e0af2ac24b49d30236482b2d64c283
eliemugenzi <eliemugenzi@gmail.com>
2018-10-30 15:38:47 +0200

    Adding news page and Fixing navigation

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -188,7 +188,7 @@ module.hot.accept(reloadCSS);
         module.hot.dispose(reloadCSS);
         module.hot.accept(reloadCSS);
       
-},{"./ajax-loader.gif":[["ajax-loader.8c8a0c6d.gif","node_modules/slick-carousel/slick/ajax-loader.gif"],"node_modules/slick-carousel/slick/ajax-loader.gif"],"./fonts/slick.eot":[["slick.c929d11d.eot","node_modules/slick-carousel/slick/fonts/slick.eot"],"node_modules/slick-carousel/slick/fonts/slick.eot"],"./fonts/slick.woff":[["slick.163147d7.woff","node_modules/slick-carousel/slick/fonts/slick.woff"],"node_modules/slick-carousel/slick/fonts/slick.woff"],"./fonts/slick.ttf":[["slick.2a44336d.ttf","node_modules/slick-carousel/slick/fonts/slick.ttf"],"node_modules/slick-carousel/slick/fonts/slick.ttf"],"./fonts/slick.svg":[["slick.9168fb61.svg","node_modules/slick-carousel/slick/fonts/slick.svg"],"node_modules/slick-carousel/slick/fonts/slick.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"scss/main.scss":[function(require,module,exports) {
+},{"./ajax-loader.gif":[["ajax-loader.8c8a0c6d.gif","node_modules/slick-carousel/slick/ajax-loader.gif"],"node_modules/slick-carousel/slick/ajax-loader.gif"],"./fonts\\slick.eot":[["slick.c929d11d.eot","node_modules/slick-carousel/slick/fonts/slick.eot"],"node_modules/slick-carousel/slick/fonts/slick.eot"],"./fonts\\slick.woff":[["slick.163147d7.woff","node_modules/slick-carousel/slick/fonts/slick.woff"],"node_modules/slick-carousel/slick/fonts/slick.woff"],"./fonts\\slick.ttf":[["slick.2a44336d.ttf","node_modules/slick-carousel/slick/fonts/slick.ttf"],"node_modules/slick-carousel/slick/fonts/slick.ttf"],"./fonts\\slick.svg":[["slick.9168fb61.svg","node_modules/slick-carousel/slick/fonts/slick.svg"],"node_modules/slick-carousel/slick/fonts/slick.svg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"scss/main.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -220,7 +220,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50979" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55340" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- scripts/Menu.js
+++ scripts/Menu.js
@@ -1,8 +1,31 @@
-let menuOpenButton = document.querySelector('.menu__button'),
-    mobileMenuWrapper = document.querySelector('.mobile-nav');
+let menuOpenButton = document.querySelector(".menu__button"),
+  mobileMenuWrapper = document.querySelector(".mobile-nav");
+const w = window.innerWidth;
+let menuWithSubmenu = document.querySelectorAll(".menu__item--with-submenu");
+console.log(w);
 
-menuOpenButton.addEventListener('click', revealMenu);
+if (w >= 1024) {
+  menuWithSubmenu.forEach(menu => {
+    menu.addEventListener("mouseover", e => {
+      e.preventDefault();
+      menu.classList.add("open");
+    });
+    menu.addEventListener("mouseleave", e => {
+      e.preventDefault();
+      menu.classList.remove("open");
+    });
+  });
+} else {
+  menuWithSubmenu.forEach(menu => {
+    menu.addEventListener("click", e => {
+      e.preventDefault();
+      menu.classList.toggle("open");
+    });
+  });
+}
+
+menuOpenButton.addEventListener("click", revealMenu);
 
 let revealMenu = () => {
-    mobileMenuWrapper.classList['add']('is-visible');
-}
+  mobileMenuWrapper.classList["add"]("is-visible");
+};
